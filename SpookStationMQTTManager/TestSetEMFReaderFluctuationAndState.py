0f054259dde1e8024adf56f1d98676043f99fd97
Douglas Halse <4headmaster@gmail.com>
2023-07-20 21:54:46 +0200

    Improve fluctuation test

--- SpookStationMQTTManager/TestSetEMFReaderFluctuationAndState.py
+++ SpookStationMQTTManager/TestSetEMFReaderFluctuationAndState.py
@@ -2,6 +2,9 @@ from SpookStationManager import SpookStationManager
 from SpookStationManagerEnums import SpookStationDeviceType
 import time
 
+LINE_UP = '\033[1A'
+LINE_CLEAR = '\x1b[2K'
+
 manager = SpookStationManager()
 
 manager.addDevice("EMFReader1", SpookStationDeviceType.EMFReader)
@@ -17,4 +20,7 @@ inputFluctuationMagnitude = input("Enter desired fluctuation magnitude: ")
 manager.devices[manager.getDeviceIndex(deviceName="EMFReader1")].setFluctuationMagnitude(int(inputFluctuationMagnitude))
 
 while 1:
-    1+1
+    print(manager.devices[manager.getDeviceIndex(deviceName="EMFReader1")], end=" ")
+    print("Current state: " + str(manager.devices[manager.getDeviceIndex(deviceName="EMFReader1")].getCurrentState()))
+    time.sleep(0.01)
+    print(LINE_UP, end=LINE_CLEAR)

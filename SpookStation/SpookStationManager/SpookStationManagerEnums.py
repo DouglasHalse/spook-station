b94b8e37ec911ff519a5fe153628c87d052b187c
Douglas Halse <4headmaster@gmail.com>
2023-09-17 14:47:40 +0200

    Add manage devices menu

--- SpookStation/SpookStationManager/SpookStationManagerEnums.py
+++ SpookStation/SpookStationManager/SpookStationManagerEnums.py
@@ -3,12 +3,23 @@ from enum import Enum
 class SpookStationDeviceType(Enum):
     EMFReader = 1
     SpiritBox = 2
-    
+
+
+SpookStationDeviceTypeToString = {
+    SpookStationDeviceType.EMFReader: "EMF Reader",
+    SpookStationDeviceType.SpiritBox: "Spirit Box"
+}
+
+
+SpookStationDeviceStringToType = {v: k for k, v in SpookStationDeviceTypeToString.items()}
+
+
 class SpookStationDeviceConnectionState(Enum):
 	Connected = 1
 	PoorConnection = 2
 	Disconnected = 3
-        
+
+
 class SpookStationSignalType(Enum):
       State = 1
       Control = 2
